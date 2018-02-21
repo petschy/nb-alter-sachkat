5dea5499540c2e796bd6b9245f1cc96bc0446855
petschy <petschy@bluewin.ch>
2018-02-21 13:32:28 +0100

    Subroutinen für Unterfelder

--- alterSachkat.pl
+++ alterSachkat.pl
@@ -21,11 +21,14 @@ my $fh_960  = IO::File->new( $file_960, '>:utf8' );
 my $fh_961  = IO::File->new( $file_961, '>:utf8' );
 my $fh_968  = IO::File->new( $file_968, '>:utf8' );
 
-# headers
+##########################
+# header of output files #
+##########################
+
 my $bib_id         = "BIB_ID";
 my $i1             = "I1";
 my $i2             = "I2";
-my $heading        = "Feldinhalt";
+my $heading        = "Heading";
 my $field_content  = "Feldinhalt mit UF-Codierung";
 my $sf_a           = "UF a";
 my $sf_b           = "UF b";
@@ -34,6 +37,7 @@ my $sf_d           = "UF d";
 my $sf_x           = "UF x";
 my $sf_y           = "UF y";
 my $sf_z           = "UF z";
+my $sf_9           = "UF 9";
 my $sf_other       = "andere UF";
 my @output_columns = ();
 my $output         = "";
@@ -56,13 +60,16 @@ say $fh_961 $output;
 
 # Field 968
 @output_columns = (
-	$bib_id, $i1,   $i2,   "DK",  $field_content,
-	$sf_a,   $sf_x, $sf_y, $sf_z, $sf_other
+	$bib_id, $i1,   $i2,   "DK",  $field_content, $sf_a,
+	$sf_x,   $sf_y, $sf_z, $sf_9, $sf_other
 );
 $output = join( "\t", @output_columns );
 say $fh_968 $output;
 
-# MARC records
+################
+# MARC records #
+################
+
 my $record_counter = 1;
 my $records        = MARC::File::USMARC->in($fh_marc);
 while ( my $record = $records->next() ) {
@@ -71,7 +78,17 @@ while ( my $record = $records->next() ) {
 	$bib_id =~ s/^vtls//g;
 	$bib_id = $bib_id;
 
-	# Reset values for output
+	# Console output
+	say "Record no. $record_counter:\t$bib_id";
+
+	# Lists of 96x fields
+	my @fields_960 = $record->field('960');
+	my @fields_961 = $record->field('961');
+	my @fields_968 = $record->field('968');
+
+	# Field 960
+	# ---------
+	# Reset subfield data
 	$i1            = "";
 	$i2            = "";
 	$heading       = "";
@@ -83,21 +100,18 @@ while ( my $record = $records->next() ) {
 	$sf_x          = "";
 	$sf_y          = "";
 	$sf_z          = "";
+	$sf_9          = "";
 	$sf_other      = "";
-
-	# Console output
-	say "Record no. $record_counter:\t$bib_id";
-
-	# Lists of 96x fields
-	my @fields_960 = $record->field('960');
-	my @fields_961 = $record->field('961');
-	my @fields_968 = $record->field('968');
-
 	foreach my $field (@fields_960) {
 
 		$i1 = $field->indicator(1);
 		$i2 = $field->indicator(2);
-		my $field_content = $field->as_string();
+		$heading = lc $field->as_string();
+		$heading =~ s/(\(|\))//g;
+		$heading =~ s/ ; |, / /g;
+		my @subfields = $field->subfields();
+		( $sf_a, $sf_b, $sf_c, $sf_d, $sf_other ) = _subfields_960(@subfields);
+		$field_content = _field_content(@subfields);
 
 		# File output
 		@output_columns = (
@@ -109,39 +123,76 @@ while ( my $record = $records->next() ) {
 		say $fh_960 $output;
 	}
 
+	# Field 961
+	# ---------
+	# Reset subfield data
+	$i1            = "";
+	$i2            = "";
+	$heading       = "";
+	$field_content = "";
+	$sf_a          = "";
+	$sf_b          = "";
+	$sf_c          = "";
+	$sf_d          = "";
+	$sf_x          = "";
+	$sf_y          = "";
+	$sf_z          = "";
+	$sf_9          = "";
+	$sf_other      = "";
 	foreach my $field (@fields_961) {
 
 		my $str = "";
-		$i1 = $field->indicator(1);
-		$i2 = $field->indicator(2);
-		$heading = $field->as_string();
-		$sf_a = $field->subfield('a');
-		my @subfields     = $field->subfields();
-		$field_content = _field_content( @subfields );
+		$i1      = $field->indicator(1);
+		$i2      = $field->indicator(2);
+		$heading = $field->subfield('a');
+		$heading =~ s/(\(|\))//g;
+		my @subfields = $field->subfields();
+		( $sf_a, $sf_x, $sf_y, $sf_other ) = _subfields_961(@subfields);
+		$field_content = _field_content(@subfields);
 
 		# File output
 		@output_columns = (
-			$bib_id,        $i1,   $i2,   $heading,
-			$field_content, $sf_a, $sf_b, $sf_c,
-			$sf_d,          $sf_other
+			$bib_id,  $i1,            $i2,
+			$heading, $field_content, $sf_a,
+			$sf_x,    $sf_y, $sf_other
 		);
 		$output = join( "\t", @output_columns );
 		say $fh_961 $output;
 	}
-	
+
+	# Field 968
+	# ---------
+	# Reset subfield data
+	$i1            = "";
+	$i2            = "";
+	$heading       = "";
+	$field_content = "";
+	$sf_a          = "";
+	$sf_b          = "";
+	$sf_c          = "";
+	$sf_d          = "";
+	$sf_x          = "";
+	$sf_y          = "";
+	$sf_z          = "";
+	$sf_9          = "";
+	$sf_other      = "";
 	foreach my $field (@fields_968) {
 
-		$i1 = $field->indicator(1);
-		$i2 = $field->indicator(2);
-		my $field_content = $field->as_string();
-
-		#				_subfields( $field->subfields() );
+		$i1      = $field->indicator(1);
+		$i2      = $field->indicator(2);
+		$heading = $field->subfield('9');
+		$heading =~ s/^ \| //;
+		$heading =~ s/<-> //g;
+		my @subfields = $field->subfields();
+		( $sf_a, $sf_x, $sf_y, $sf_z, $sf_9, $sf_other ) =
+		  _subfields_968(@subfields);
+		$field_content = _field_content(@subfields);
 
 		# File output
 		@output_columns = (
 			$bib_id,        $i1,   $i2,   $heading,
-			$field_content, $sf_a, $sf_b, $sf_c,
-			$sf_d,          $sf_other
+			$field_content, $sf_a, $sf_x, $sf_y,
+			$sf_z,          $sf_9, $sf_other
 		);
 		$output = join( "\t", @output_columns );
 		say $fh_968 $output;
@@ -149,30 +200,124 @@ while ( my $record = $records->next() ) {
 	$record_counter++;
 }
 
+# Feldinhalt mit Unterfeldcodierungen
 sub _field_content {
 	my @subfields = @_;
 	my $str       = "";
 	while ( my $subfield = shift(@subfields) ) {
-			my ( $code, $data ) = @$subfield;
-			say "Code: $code; Data: $data";
-			$str = "$str \$$code $data";
+		my ( $code, $data ) = @$subfield;
+		say "Code: $code; Data: $data";
+		$str = "$str \$$code $data";
 	}
 	return trim($str);
 }
 
-sub _subfields {
+
+sub _subfields_960 {
 	my @subfields = @_;
-	my $str       = "";
-
-	#	say Dumper @subfields;
+	my $sf_a      = "";
+	my $sf_b      = "";
+	my $sf_c      = "";
+	my $sf_d      = "";
+	my $sf_other  = "";
 	while ( my $subfield = shift(@subfields) ) {
-			my ( $code, $data ) = @$subfield;
-			say "Code: $code; Data: $data";
-			$str = "$str \$$code $data";
-
-		#		say Dumper @$subfield;
-
+		my ( $code, $data ) = @$subfield;
+		say "-->961 Code: $code; Data: $data";
+		if ( $code eq 'a' ) {
+			$sf_a = "$sf_a | $data";
+			say "--> a: $sf_b";
+		}
+		elsif ( $code eq 'b' ) {
+			$sf_b = "$sf_b | $data";
+			say "--> x: $sf_b";
+		}
+		elsif ( $code eq 'c' ) {
+			$sf_c = "$sf_c | $data";
+			say "--> y: $sf_c";
+		}
+		elsif ( $code eq 'd' ) {
+			$sf_d = "$sf_d | $data";
+			say "--> y: $sf_c";
+		}
+		else {
+			$sf_other = "$sf_other | $data";
+		}
 	}
-	return trim($str);
+	$sf_a =~ s/^ \| //;
+	$sf_b =~ s/^ \| //;
+	$sf_c =~ s/^ \| //;
+	$sf_d =~ s/^ \| //;
+	$sf_other =~ s/^ \| //;
+	my @arr = ( $sf_a, $sf_b, $sf_c, $sf_d, $sf_other );
+	return @arr;
+}
+
+sub _subfields_961 {
+	my @subfields = @_;
+	my $sf_a      = "";
+	my $sf_x      = "";
+	my $sf_y      = "";
+	my $sf_other  = "";
+	while ( my $subfield = shift(@subfields) ) {
+		my ( $code, $data ) = @$subfield;
+		if ( $code eq 'a' ) {
+			$sf_a = "$sf_a | $data";
+		}
+		elsif ( $code eq 'x' ) {
+			$sf_x = "$sf_x | $data";
+		}
+		elsif ( $code eq 'y' ) {
+			$sf_y = "$sf_y | $data";
+		}
+		else {
+			$sf_other = "$sf_other | $data";
+		}
+	}
+	$sf_a =~ s/^ \| //;
+	$sf_x =~ s/^ \| //;
+	$sf_y =~ s/^ \| //;
+	$sf_other =~ s/^ \| //;
+	my @arr = ( $sf_a, $sf_x, $sf_y, $sf_other );
+	return @arr;
+}
+
+sub _subfields_968 {
+	my @subfields = @_;
+	my $sf_a      = "";
+	my $sf_x      = "";
+	my $sf_y      = "";
+	my $sf_z      = "";
+	my $sf_9      = "";
+	my $sf_other  = "";
+	while ( my $subfield = shift(@subfields) ) {
+		my ( $code, $data ) = @$subfield;
+		say "Code: $code; Data: $data";
+		if ( $code eq 'a' ) {
+			$sf_a = "$sf_a | $data";
+		}
+		elsif ( $code eq 'x' ) {
+			$sf_x = "$sf_x | $data";
+		}
+		elsif ( $code eq 'y' ) {
+			$sf_y = "$sf_y | $data";
+		}
+		elsif ( $code eq 'z' ) {
+			$sf_z = "$sf_z | $data";
+		}
+		elsif ( $code eq '9' ) {
+			$sf_9 = "$sf_9 | $data";
+		}
+		else {
+			$sf_other = "$sf_other | $data";
+		}
+	}
+	$sf_a =~ s/^ \| //;
+	$sf_x =~ s/^ \| //;
+	$sf_y =~ s/^ \| //;
+	$sf_z =~ s/^ \| //;
+	$sf_9 =~ s/^ \| //;
+	$sf_other =~ s/^ \| //;
+	my @arr = ( $sf_a, $sf_x, $sf_y, $sf_z, $sf_9, $sf_other );
+	return @arr;
 }
 
